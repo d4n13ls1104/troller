0991a716f2cca65a388274ba7bccf652382e532e
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2021-12-17 17:58:57 -0800

    feat: login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,34 @@
+import Link from "components/common/Link";
+import Button from "components/FormComponents/Button";
+import FormHeaderText from "components/FormComponents/FormHeaderText";
+import FormHeaderWrapper from "components/FormComponents/FormHeaderWrapper";
+import FormInputWrapper from "components/FormComponents/FormInputWrapper";
+import FormText from "components/FormComponents/FormText";
 import FormNavWrapper from "components/FormComponents/FormWrapper";
-import React from "react";
+import Input from "components/FormComponents/Input";
+import React, { useEffect } from "react";
+import { COLORS } from "util/consts";
 
 
 
 const Login: React.FC = () => {
+    useEffect(() => {
+        document.body.style.backgroundColor = COLORS.BACKGROUND;
+    })
+
     return (
         <>
             <FormNavWrapper>
-                e
+                <FormHeaderWrapper>
+                    <FormHeaderText>Log In</FormHeaderText>
+                </FormHeaderWrapper>
+                <FormInputWrapper>
+                    <Input type="email" placeholder="example@example.com"/>
+                    <Input type="password" placeholder="Password"/>
+                    <Button>Log In</Button>
+                    <FormText>Don't have an account? <Link href="/register">Sign up</Link>!</FormText>
+                    <Link href="/">Forgot Password</Link>
+                </FormInputWrapper>
             </FormNavWrapper>
         </>
     );
