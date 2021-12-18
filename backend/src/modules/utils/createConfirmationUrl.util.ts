7d74e37012132be9238e9cd8ce45c14b832dc0f3
Andrew Daniels <markassbrownly555@gmail.com>
2021-12-17 20:34:40 -0500

    add function to create token for user and confirmation url

---
+++ backend/src/modules/utils/createConfirmationUrl.util.ts
@@ -0,0 +1,10 @@
+import { v4 } from "uuid";
+import { redis } from "redis";
+import { confirmUserPrefix } from "modules/constants/redisPrefixes";
+
+export const createConfirmationUrl = async (userId: string) => {
+  const token = v4();
+  await redis.set(confirmUserPrefix + token, userId, "ex", 60 * 60 * 24);
+
+  return process.env.BASE_URL + `/user/confirm/${token}`;
+};
