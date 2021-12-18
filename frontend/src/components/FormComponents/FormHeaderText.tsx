5c70a4d719affe8173119c23ad470986f90620f3
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2021-12-17 17:58:07 -0800

    feat: login page components

---
+++ frontend/src/components/FormComponents/FormHeaderText.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const FormHeaderText = styled.h1`
+    font-size: 32px;
+`
+
+export default FormHeaderText;
---
+++ frontend/src/components/FormComponents/FormText.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const FormText = styled.p`
+    font-size: 16px;
+`
+
+export default FormText;
